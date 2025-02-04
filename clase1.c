c396988e0b064ea235a4fce40985ec90ff298d51
Peloponeso31 <tanilizquierdo@proton.me>
2025-02-04 10:56:24 -0600

    first commit

---
+++ clase1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+// argc: Argument count.
+// argv: Argument vector.
+int main(int argc, char * argv[])
+{
+  // Parentesis
+  // Exponentes
+  // Multiplicacion
+  // Division
+  // Addition (Suma)
+  // Subtraction (Resta)
+  
+  int a = 123;
+  int b = 78;
+  
+  printf("Resultado de %i + %i = %i\n", a, b, a+b);
+  printf("Resultado de opreacion: %i\n", 3+3*(3+3) );
+  return 0;
+}
+
+
